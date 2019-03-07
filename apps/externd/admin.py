1b24357ecd889833250132dcea4d68241dff9bbb
yanzongzhen <2023335616@qq.com>
2019-03-07 11:37:15 +0800

    masking

--- apps/externd/admin.py
+++ apps/externd/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from externd.models import  DataMaskingColumns, DataMaskingRules
+# 脱敏字段页面定义
+
+
+@admin.register(DataMaskingColumns)
+class DataMaskingColumnsAdmin(admin.ModelAdmin):
+    list_display = (
+        'column_id', 'rule_type', 'active', 'instance_name', 'table_schema', 'table_name', 'column_name',
+        'create_time',)
+    search_fields = ['table_name', 'column_name']
+    list_filter = ('rule_type', 'active', 'instance_name')
+
+
+# 脱敏规则页面定义
+@admin.register(DataMaskingRules)
+class DataMaskingRulesAdmin(admin.ModelAdmin):
+    list_display = (
+        'rule_type', 'rule_regex', 'hide_group', 'rule_desc', 'sys_time',)
--- apps/externd/models.py
+++ apps/externd/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+# 脱敏字段配置
+class DataMaskingColumns(models.Model):
+    column_id = models.AutoField('字段id', primary_key=True)
+    rule_type = models.IntegerField('规则类型',
+                                    choices=((1, '手机号'), (2, '证件号码'), (3, '银行卡'), (4, '邮箱'), (5, '金额'), (6, '其他')))
+    active = models.IntegerField('激活状态', choices=((0, '未激活'), (1, '激活')))
+    instance_name = models.CharField('实例名称', max_length=50)
+    table_schema = models.CharField('字段所在库名', max_length=64)
+    table_name = models.CharField('字段所在表名', max_length=64)
+    column_name = models.CharField('字段名', max_length=64)
+    column_comment = models.CharField('字段描述', max_length=1024, default='', blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    sys_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = True
+        db_table = 'sqlaudit_data_masking_columns'
+        verbose_name = u'脱敏字段配置'
+        verbose_name_plural = u'脱敏字段配置'
+
+
+# 脱敏规则配置
+class DataMaskingRules(models.Model):
+    rule_type = models.IntegerField('规则类型',
+                                    choices=((1, '手机号'), (2, '证件号码'), (3, '银行卡'), (4, '邮箱'), (5, '金额'), (6, '其他')),
+                                    unique=True)
+    rule_regex = models.CharField('规则脱敏所用的正则表达式，表达式必须分组，隐藏的组会使用****代替', max_length=255)
+    hide_group = models.IntegerField('需要隐藏的组')
+    rule_desc = models.CharField('规则描述', max_length=100, default='', blank=True)
+    sys_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = True
+        db_table = 'sqlaudit_data_masking_rules'
+        verbose_name = u'脱敏规则配置'
+        verbose_name_plural = u'脱敏规则配置'
